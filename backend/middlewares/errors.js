3d1f67dda68333ade2aa5c00d37bf7151ee9cbb1
Наталья <mrs.solts@mail.ru>
2023-06-01 02:21:23 +0300

    fix: errors.js

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -1,49 +1,58 @@
-const {
-  ValidationError,
-  DocumentNotFoundError,
-  CastError,
-} = require('mongoose').Error;
+// const {
+//   ValidationError,
+//   DocumentNotFoundError,
+//   CastError,
+// } = require('mongoose').Error;
 
 const {
-  BAD_REQUEST_ERROR,
-  NOT_FOUND_ERROR,
-  CONFLICT_ERROR,
+  // BAD_REQUEST_ERROR,
+  // NOT_FOUND_ERROR,
+  // CONFLICT_ERROR,
   DEFAULT_ERROR,
 } = require('../utils/constants');
 
-const UnauthorizedError = require('../utils/errors/UnauthorizedError');
-const ForbiddenError = require('../utils/errors/ForbiddenError');
-const NotFoundError = require('../utils/errors/NotFoundError');
+// const UnauthorizedError = require('../utils/errors/UnauthorizedError');
+// const ForbiddenError = require('../utils/errors/ForbiddenError');
+// const NotFoundError = require('../utils/errors/NotFoundError');
 
-module.exports = ((err, req, res, next) => {
-  if (err instanceof UnauthorizedError || ForbiddenError || NotFoundError) {
-    return res.status(err.statusCode).send({
-      message: err.message,
-    });
-  }
-  if (err instanceof ValidationError) {
-    const errorMessage = Object.values(err.errors).map((error) => error.message).join(' ');
-    return res.status(BAD_REQUEST_ERROR).send({
-      message: `Переданы некорректные данные. ${errorMessage}`,
-    });
-  }
-  if (err instanceof DocumentNotFoundError) {
-    return res.status(NOT_FOUND_ERROR).send({
-      message: `Пользователь с указанным id не найден: ${err.value}`,
-    });
-  }
-  if (err instanceof CastError) {
-    return res.status(BAD_REQUEST_ERROR).send({
-      message: 'Переданы некорректные данные.',
-    });
-  }
-  if (err.code === 11000) {
-    return res.status(CONFLICT_ERROR).send({
-      message: 'Пользователь с таким email уже зарегистрирован.',
-    });
-  }
-  res.status(DEFAULT_ERROR).send({
-    message: 'На сервере произошла ошибка.',
+module.exports = (err, req, res, next) => {
+  const statusCode = err.statusCode || DEFAULT_ERROR;
+  const errorMessage = statusCode === DEFAULT_ERROR ? `Произошла неизвестная ошибка ${err.name}: ${err.message}` : err.message;
+  res.status(statusCode).send({
+    message: errorMessage,
   });
   return next();
-});
+};
+
+// module.exports = ((err, req, res, next) => {
+//   if (err instanceof UnauthorizedError || ForbiddenError || NotFoundError) {
+//     return res.status(err.statusCode).send({
+//       message: err.message,
+//     });
+//   }
+//   if (err instanceof ValidationError) {
+//     const errorMessage = Object.values(err.errors).map((error) => error.message).join(' ');
+//     return res.status(BAD_REQUEST_ERROR).send({
+//       message: `Переданы некорректные данные. ${errorMessage}`,
+//     });
+//   }
+//   if (err instanceof DocumentNotFoundError) {
+//     return res.status(NOT_FOUND_ERROR).send({
+//       message: `Пользователь с указанным id не найден: ${err.value}`,
+//     });
+//   }
+//   if (err instanceof CastError) {
+//     return res.status(BAD_REQUEST_ERROR).send({
+//       message: 'Переданы некорректные данные.',
+//     });
+//   }
+//   if (err.code === 11000) {
+//     return res.status(CONFLICT_ERROR).send({
+//       message: 'Пользователь с таким email уже зарегистрирован.',
+//     });
+//   }
+//   res.status(DEFAULT_ERROR).send({
+//     message: 'На сервере произошла ошибка.',
+//   });
+//   return next();
+// });
