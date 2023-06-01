54a874737b459fdbe0d16269a09d019da39519c4
Наталья <mrs.solts@mail.ru>
2023-06-01 13:59:02 +0300

    fix

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -66,13 +66,3 @@ module.exports = ((err, req, res, next) => {
   });
   return next();
 });
-
-// module.exports = (err, req, res, next) => {
-//   const statusCode = err.statusCode || DEFAULT_ERROR;
-//   const errorMessage = statusCode === DEFAULT_ERROR ?
-// `Произошла неизвестная ошибка ${err.name}: ${err.message}` : err.message;
-//   res.status(statusCode).send({
-//     message: errorMessage,
-//   });
-//   return next();
-// };
