9c0baa0095b7695939ffd293062fc53190354d70
Наталья <mrs.solts@mail.ru>
2023-06-02 23:58:48 +0300

    ref: изменить импорты routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,15 +1,19 @@
 const router = require('express').Router();
 
+const signin = require('./signin');
+const signup = require('./signup');
+const users = require('./users');
+const cards = require('./cards');
+const NotFoundError = require('../utils/errors/NotFoundError');
 const { logout } = require('../controllers/users');
 const auth = require('../middlewares/auth');
-const NotFoundError = require('../utils/errors/NotFoundError');
 
-router.use('/signin', require('./signin'));
-router.use('/signup', require('./signup'));
+router.use('/signin', signin);
+router.use('/signup', signup);
 
 router.post('/sign-out', logout);
-router.use('/users', auth, require('./users'));
-router.use('/cards', auth, require('./cards'));
+router.use('/users', auth, users);
+router.use('/cards', auth, cards);
 
 router.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
