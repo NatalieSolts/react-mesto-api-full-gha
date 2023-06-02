b5c3337aebf2a540ccdc199c64d4d8a46d1f95c9
Наталья <mrs.solts@mail.ru>
2023-06-03 00:18:47 +0300

    test

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -128,7 +128,7 @@ function App() {
   function handleCardLike(card) {
     const isLiked = card.likes.some((i) => i._id === currentUser._id); // проверяем, есть ли уже лайк на этой карточке
     api // Отправляем запрос в API и получаем обновлённые данные карточки
-      .changeLikeCardStatus(card._id, isLiked)
+      .changeLikeCardStatus(card._id, !isLiked)
       .then((newCard) => {
         setCards((state) =>
           state.map((c) => (c._id === card._id ? newCard : c))
