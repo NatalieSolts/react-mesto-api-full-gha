ccd2d52b0138eb4fdfb330addf1005ed014a0e63
Наталья <mrs.solts@mail.ru>
2023-06-02 20:27:38 +0300

    fix: редактировать back/middlewares/auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,14 +2,18 @@ const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../utils/errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
-  if (!token) {
+  const cookie = req.cookies.jwt;
+  if (!cookie) {
     return next(new UnauthorizedError('Необходима авторизация'));
   }
+  const token = cookie.replace('jwt', '');
   let payload;
   try {
     payload = jwt.verify(token, process.env.NODE_ENV === 'production' ? process.env.SECRET_KEY : 'secret-key');
   } catch (err) {
+    if (err.name === 'JsonWebTokenError') {
+      next(UnauthorizedError('Некорректный токен'));
+    }
     return next(UnauthorizedError('Необходима авторизация'));
   }
   req.user = payload;
